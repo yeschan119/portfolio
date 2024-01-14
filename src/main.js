b4f75c4cd7857a62223ce8fe4ee60a0e6cb27586
eungchan-kang <yeschan119@gmail.com>
2024-01-14 14:38:53 +0900

    js-arrow-up

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 //comments
-//get height of header
+//투명상태에 있던 header bar를 header heigtht만큼 아래로 내리면 보여주기
 const header = document.querySelector('.header');
 const headerHeight = header.getBoundingClientRect().height;
-
-
-//get event whenever scrolling
 document.addEventListener('scroll', () => {
     if (window.scrollY > headerHeight - 10) {
         header.classList.add('header--dark');
@@ -14,8 +11,20 @@ document.addEventListener('scroll', () => {
     }
 });
 
+//스크롤을 아래로 내리면 home 부분이 점점 사라지기
 const home = document.querySelector('.home__container');
 const homeHeight = home.offsetHeight;
 document.addEventListener('scroll', () => {
     home.style.opacity = 1 - window.scrollY / homeHeight;
+})
+
+//home에서는 home으로 올라가는 화살표가 보이지 않기
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if (window.scrollY <= homeHeight/2) {
+        arrowUp.style.opacity = 0;
+    }
+    else {
+        arrowUp.style.opacity = 1;
+    }
 })
