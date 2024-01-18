b4aa9b2564d56c90317dc5b507454ec0b6eea6f3
eungchan-kang <yeschan119@gmail.com>
2024-01-19 01:47:32 +0900

    debug

--- src/type.js
+++ src/type.js
@@ -1,19 +1,19 @@
 'use strict';
 
-new TypeIt('.home__title--strong', {
+new TypeIt('.home__title--name', {
     loop: true,
     speed: 100,
 }) // Eungchan
-.move(-9) // |
+.move(-8) // |
 .type('front-end engineer, ') // front-end engineer|
 .pause(1000)
 .move(-10)  // front-end| engineer
 .delete(10)  // | engineer
 .type('back-end ')  // back-end| engineer
 .pause(1000)
-.delete(10) //| engineer
+.delete(9) //| engineer
 .type('full-stack ') //full-stack| engineer
 .pause(1000)
-.move(19)
+.move(18)
 .delete()
 .go();
