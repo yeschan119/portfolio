a7293d167589076d6d32c470437fcc7302359626
eungchan-kang <yeschan119@gmail.com>
2024-01-21 10:34:04 +0900

    dd

--- src/type.js
+++ src/type.js
@@ -5,7 +5,7 @@ new TypeIt('.home__title--name', {
     speed: 100,
 }) // Eungchan
 .move(-8) // |
-.type('front-end engineer, ') // front-end engineer|
+.type('a front-end engineer, ') // front-end engineer|
 .pause(1000)
 .move(-10)  // front-end| engineer
 .delete(10)  // | engineer
