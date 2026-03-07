148a568c34fe1e4d4154700069ab388e3e56fefb
Kane <Kane@kokomo247.com>
2026-03-07 11:11:27 +0900

    type

--- src/type.js
+++ src/type.js
@@ -1,19 +1,35 @@
 'use strict';
 
-new TypeIt('.home__title--name', {
-    loop: true,
-    speed: 100,
-}) // Eungchan
-.move(-8) // |
-.type('a front-end engineer, ') // front-end engineer|
-.pause(1000)
-.move(-10)  // front-end| engineer
-.delete(10)  // | engineer
-.type('back-end ')  // back-end| engineer
-.pause(1000)
-.delete(9) //| engineer
-.type('full-stack ') //full-stack| engineer
-.pause(1000)
-.move(18)
-.delete()
-.go();
+document.addEventListener("DOMContentLoaded", () => {
+    const selector = '.home__title--name';
+
+    function runTyping() {
+        document.querySelector(selector).innerHTML = '';
+
+        new TypeIt(selector, {
+            speed: 85,
+            loop: false,
+            cursor: true,
+            waitUntilVisible: true,
+            afterComplete: (instance) => {
+                setTimeout(() => {
+                instance.destroy();
+                runTyping();
+                }, 5000);
+            }
+        })
+        .type("I'm Eungchan Kang,")
+        .pause(600)
+        .break()
+        .type("a Full-Stack Engineer")
+        .pause(900)
+        .delete(21)
+        .type("a Cloud & Data Platform Engineer")
+        .pause(1000)
+        .delete(32)
+        .type("a Full-Stack & Data Platform Engineer.")
+        .go();
+    }
+
+    runTyping();
+});
